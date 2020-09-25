9868b0461cdcbcba3a9e6ad2352444d62dd5a478
Simas <simaskiriliauskas@gmail.com>
2020-09-26 00:24:13 +0300

    added variables sass

--- simas.kiriliauskas.frontend/src/App.js
+++ simas.kiriliauskas.frontend/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.scss';
+import AddAlertIcon from '@material-ui/icons/AddAlert';
+import AddBoxIcon from '@material-ui/icons/AddBox';
 
 function App() {
 
+  const [button, setButton] = useState({
+    buttonSwitch: true,
+    random: false
+  });
+
+  console.log(button);
+
   return (
     <div className="App">
-      Hello
+      new project
     </div>
   );
 }
