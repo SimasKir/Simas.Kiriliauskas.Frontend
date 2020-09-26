4525620ac73c06f9ac00ccf570a3ad9fff8507a1
Simas <simaskiriliauskas@gmail.com>
2020-09-26 08:36:22 +0300

    deleted unnecesary files

--- simas.kiriliauskas.frontend/src/App.js
+++ simas.kiriliauskas.frontend/src/App.js
@@ -1,16 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.scss';
-import AddAlertIcon from '@material-ui/icons/AddAlert';
-import AddBoxIcon from '@material-ui/icons/AddBox';
 
 function App() {
-
-  const [button, setButton] = useState({
-    buttonSwitch: true,
-    random: false
-  });
-
-  console.log(button);
 
   return (
     <div className="App">
